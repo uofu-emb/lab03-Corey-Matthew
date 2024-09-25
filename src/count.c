452ba52238f62782928ed3c6a7e05c005250dc71
Corey Buchanan <coreybuchanan.engineer@gmail.com>
2024-09-25 11:56:16 -0600

    Add tests for semaphore. Not working quite yet.

--- src/count.c
+++ src/count.c
@@ -1,36 +1,36 @@
+#include <stdio.h>
 #include <FreeRTOS.h>
+
 #include "count.h"
 
-bool double_plus1_and_print(int *counter, SemaphoreHandle_t *semaphore) {
+bool double_plus1_and_print(int *counter, SemaphoreHandle_t *semaphore)
+{
     int temp;
-        if (!xSemaphoreTake((*semaphore), portTICK_PERIOD_MS * 50)) return false;
-        {
-            (*counter) += (*counter) + 1;
-            temp = (*counter);
-        }
-        xSemaphoreGive((*semaphore));
+    if (!xSemaphoreTake((*semaphore), portTICK_PERIOD_MS * 50))
+        return false;
+    {
+        (*counter) += (*counter) + 1;
+        temp = (*counter);
+    }
+    xSemaphoreGive((*semaphore));
 
-		printf("hello world from %s! Count %d\n", "thread", temp);
+    printf("hello world from %s! Count %d\n", "thread", temp);
 
-        return true;
+    return true;
 }
 
-bool print_and_increment(int *counter, SemaphoreHandle_t *semaphore) {
+bool print_and_increment(int *counter, SemaphoreHandle_t *semaphore)
+{
     int temp;
-        if (!xSemaphoreTake((*semaphore), portTICK_PERIOD_MS * 50)) return false;
-        {
-            temp = (*counter);
-        }
-        xSemaphoreGive((*semaphore));
+    if (!xSemaphoreTake((*semaphore), portTICK_PERIOD_MS * 50))
+        return false;
+    {
+        temp = (*counter);
+        (*counter)++; // We want to print the pre-increment value
+    }
+    xSemaphoreGive((*semaphore));
 
-		printf("hello world from %s! Count %d\n", "main", temp);
+    printf("hello world from %s! Count %d\n", "main", temp);
 
-        if (!xSemaphoreTake((*semaphore), portTICK_PERIOD_MS * 50)) return false;
-        {
-            (*counter)++;
-        }
-        xSemaphoreGive((*semaphore));
-
-        return true;
+    return true;
 }
-
--- test/test.c
+++ test/test.c
@@ -4,24 +4,59 @@
 #include <unity.h>
 #include "unity_config.h"
 
+#include <FreeRTOS.h>
 #include "count.h"
 
 void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_variable_assignment()
-{
-    int x = 1;
-    TEST_ASSERT_TRUE_MESSAGE(x == 1,"Variable assignment failed.");
+void test_semaphore_timeout_double_plus1() {
+    SemaphoreHandle_t semaphore;
+    // Starve the test function so it times out (simulates another thread having it)
+    xSemaphoreTake(semaphore, portMAX_DELAY);
+    printf("Semaphore taken\n");
+
+    int count = 1;
+    bool success = double_plus1_and_print(&count, &semaphore);
+    printf("Returned\n");
+    xSemaphoreGive(semaphore);
+
+    TEST_ASSERT_FALSE_MESSAGE(success, "Count was not properly locked, did not timeout.");
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(1, count, "Count was modified even though it should have been locked.");
 }
 
-void test_multiplication(void)
-{
-    int x = 30;
-    int y = 6;
-    int z = x / y;
-    TEST_ASSERT_TRUE_MESSAGE(z == 5, "Multiplication of two integers returned incorrect value.");
+void test_semaphore_timeout_increment() {
+    SemaphoreHandle_t semaphore;
+    // Starve the test function so it times out (simulates another thread having it)
+    xSemaphoreTake(semaphore, portMAX_DELAY);
+
+    int count = 1;
+    bool success = print_and_increment(&count, &semaphore);
+    xSemaphoreGive(semaphore);
+
+    TEST_ASSERT_FALSE_MESSAGE(success, "Count was not properly locked, did not timeout.");
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(1, count, "Count was modified even though it should have been locked.");
+}
+
+void test_double_plus1() {
+    SemaphoreHandle_t semaphore;
+    int count = 2;
+
+    bool success = double_plus1_and_print(&count, &semaphore);
+
+    TEST_ASSERT_TRUE_MESSAGE(success, "Unsuccessful in taking semaphore.");
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(5, count, "Count not modified properly");
+}
+
+void test_increment() {
+    SemaphoreHandle_t semaphore;
+    int count = 2;
+
+    bool success = print_and_increment(&count, &semaphore);
+
+    TEST_ASSERT_TRUE_MESSAGE(success, "Unsuccessful in taking semaphore.");
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(3, count, "Count not modified properly");
 }
 
 int main (void)
@@ -30,8 +65,10 @@ int main (void)
     sleep_ms(5000); // Give time for TTY to attach.
     printf("Start tests\n");
     UNITY_BEGIN();
-    RUN_TEST(test_variable_assignment);
-    RUN_TEST(test_multiplication);
+    RUN_TEST(test_semaphore_timeout_double_plus1);
+    RUN_TEST(test_semaphore_timeout_increment);
+    RUN_TEST(test_double_plus1);
+    RUN_TEST(test_increment);
     sleep_ms(5000);
     return UNITY_END();
 }
